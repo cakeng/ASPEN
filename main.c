99604d294440c7e0c4b121fd8bc6d45121fa1ade
cakeng <cakeng@naver.com>
2023-05-09 17:11:28 +0900

    Vgg16

--- main.c
+++ main.c
@@ -18,7 +18,7 @@ int main(void)
     // // //     printf("Error: Failed to create DNN\n");
     // // //     return -1;
     // // // }
-    print_dnn_info (vgg16_dnn, 0);
+    // print_dnn_info (vgg16_dnn, 0);
     apu_save_dnn_to_file (vgg16_dnn, "data/vgg16_base.aspen");
     // aspen_dnn_t *gpt2_dnn = apu_load_dnn_from_file ("data/gpt2_base.aspen");
     // // if (bert_dnn_2 == NULL) 
@@ -32,13 +32,13 @@ int main(void)
     // nasm_t *gpt2_nasm = apu_create_transformer_nasm(vgg16_dnn, 10e6, 100, 1, 128);
     // nasm_t *resnet50_nasm = apu_create_nasm(resnet50_dnn, 100e6, 100, 32);
     // nasm_t *yolov3_nasm = apu_create_nasm(yolov3_dnn, 100e6, 100, 1);
-    nasm_t *vgg16_nasm = apu_create_nasm(vgg16_dnn, 100e6, 10, 2);
+    nasm_t *vgg16_nasm = apu_create_nasm(vgg16_dnn, 100e6, 100, 8);
     // // if (bert_nasm == NULL) 
     // // {
     // //     printf("Error: Failed to create NASM\n");
     // //     return -1;
     // // }
-    print_nasm_info(vgg16_nasm, 1, 0);
+    // print_nasm_info(vgg16_nasm, 1, 0);
     char nasm_file_name [1024] = {0};
     // sprintf (nasm_file_name, "data/vgg16_124M_S%d_B%d_M%d_%2.1e.nasm"
     //     , gpt2_nasm->tr_seq_len, gpt2_nasm->batch_size, gpt2_nasm->min_ninst_per_ldata,
@@ -59,7 +59,7 @@ int main(void)
     // // // nasm_t *bert_4_nasm = apu_load_nasm_from_file ("data/bert_4.nasm", &bert_dnn);
     // 
     rpool_t *rpool = rpool_init (gpu);
-    ase_group_t *ase_group = ase_group_init (64, gpu);
+    ase_group_t *ase_group = ase_group_init (32, gpu);
     ase_group_set_rpool (ase_group, rpool);
 
     // // // rpool_add_nasm_raw_input (rpool, bert_4_nasm, 0.5, dog_data);
@@ -89,87 +89,88 @@ int main(void)
     // // print_rpool_info (rpool);
     // print_nasm_cudagraph_info (bert_nasm, "cudagraph_out.txt");
 
-    // gpt2_dnn->layers[7].tensors[WEIGHT_TENSOR]->data = aspen_calloc (1600*1600,4);
-    unsigned int input_params[NUM_PARAM_ELEMENTS] = {0};
-    // input_params[BATCH] = 1; input_params[NUM_SEQ] = 128; input_params[NUM_HIDDEN] = 768;
-    input_params[BATCH] = 2; input_params[OUT_C] = 3; input_params[OUT_H] = 224; input_params[OUT_W] = 224;
-    // void *dog_data = aspen_load_input ("data/batched_input_128.bin", input_params, sizeof(float));
-    void *dog_data = aspen_load_input_NHWC ("data/batched_input_128.bin", input_params, sizeof(float));
-    aspen_init_naive (vgg16_dnn, input_params, dog_data, gpu);
-    get_elapsed_time ("init_naive");
-    aspen_run_naive (vgg16_dnn, input_params, dog_data, gpu);
-    get_elapsed_time ("run_naive");
-    // print_dnn_info (gpt2_dnn, 0);
-    for (int i = 21; i < 22; i++)
-    {
-        printf ("\tLayer %d - Type %s\n", i, layer_type_str[vgg16_dnn->layers[i].type]);
-        aspen_layer_t *layer = &vgg16_dnn->layers[i];
-        nasm_ldata_t *ldata = &vgg16_nasm->ldata_arr[i];
-        assert (ldata->layer == layer);
-        LAYER_PARAMS output_order[] = {BATCH, MAT_N, MAT_M, 0};
-        LAYER_PARAMS output_order_nhwc[] = {BATCH, OUT_H, OUT_W, OUT_C};
-        LAYER_PARAMS output_order_nchw[] = {BATCH, OUT_C, OUT_H, OUT_W};
-        if (layer->type == K_ATTENTION_LAYER)
-        {
-            output_order [1] = NUM_HEAD; 
-            output_order [2] = MAT_N; 
-            output_order [3] = MAT_M;
-        }
-        void *layer_output = get_aspen_tensor_data 
-            (layer->tensors[OUTPUT_TENSOR], output_order_nchw, gpu);
-        void *ldata_output = get_ldata_output (ldata, output_order_nchw);
-        // void *ldata_raw_output = get_packed_ldata_output_colwise (ldata);
-        char filename[256];
-        sprintf (filename, "data/vgg16/vgg16_out.bin");
-        // size_t elem_size = ldata->layer->dnn->element_size;
-        // size_t data_size = ldata->out_mat_dims[OUT_H] * ldata->out_mat_dims[OUT_W] * elem_size;
-        size_t elem_size = layer->dnn->element_size;
-        size_t data_size = layer->tensors[OUTPUT_TENSOR]->num_elements * elem_size;
-        void *expected_output = load_arr (filename, data_size);
+    // // gpt2_dnn->layers[7].tensors[WEIGHT_TENSOR]->data = aspen_calloc (1600*1600,4);
+    // unsigned int input_params[NUM_PARAM_ELEMENTS] = {0};
+    // // input_params[BATCH] = 1; input_params[NUM_SEQ] = 128; input_params[NUM_HIDDEN] = 768;
+    // input_params[BATCH] = 2; input_params[OUT_C] = 3; input_params[OUT_H] = 224; input_params[OUT_W] = 224;
+    // // void *dog_data = aspen_load_input ("data/batched_input_128.bin", input_params, sizeof(float));
+    // void *dog_data = aspen_load_input_NHWC ("data/batched_input_128.bin", input_params, sizeof(float));
+    // aspen_init_naive (vgg16_dnn, input_params, dog_data, gpu);
+    // get_elapsed_time ("init_naive");
+    // aspen_run_naive (vgg16_dnn, input_params, dog_data, gpu);
+    // get_elapsed_time ("run_naive");
+    // // print_dnn_info (gpt2_dnn, 0);
+    // for (int i = 21; i < 22; i++)
+    // {
+    //     printf ("\tLayer %d - Type %s\n", i, layer_type_str[vgg16_dnn->layers[i].type]);
+    //     aspen_layer_t *layer = &vgg16_dnn->layers[i];
+    //     nasm_ldata_t *ldata = &vgg16_nasm->ldata_arr[i];
+    //     assert (ldata->layer == layer);
+    //     LAYER_PARAMS output_order[] = {BATCH, MAT_N, MAT_M, 0};
+    //     LAYER_PARAMS output_order_nhwc[] = {BATCH, OUT_H, OUT_W, OUT_C};
+    //     LAYER_PARAMS output_order_nchw[] = {BATCH, OUT_C, OUT_H, OUT_W};
+    //     if (layer->type == K_ATTENTION_LAYER)
+    //     {
+    //         output_order [1] = NUM_HEAD; 
+    //         output_order [2] = MAT_N; 
+    //         output_order [3] = MAT_M;
+    //     }
+    //     void *layer_output = get_aspen_tensor_data 
+    //         (layer->tensors[OUTPUT_TENSOR], output_order_nchw, gpu);
+    //     void *ldata_output = get_ldata_output (ldata, output_order_nchw);
+    //     // void *ldata_raw_output = get_packed_ldata_output_colwise (ldata);
+    //     char filename[256];
+    //     sprintf (filename, "data/vgg16/vgg16_out.bin");
+    //     // size_t elem_size = ldata->layer->dnn->element_size;
+    //     // size_t data_size = ldata->out_mat_dims[OUT_H] * ldata->out_mat_dims[OUT_W] * elem_size;
+    //     size_t elem_size = layer->dnn->element_size;
+    //     size_t data_size = layer->tensors[OUTPUT_TENSOR]->num_elements * elem_size;
+    //     void *expected_output = load_arr (filename, data_size);
 
-        // printf ("Expected output for layer %d:\n", i);
-        // print_float_tensor (expected_output, input_params[BATCH], layer->params[NUM_HEAD]
-        //     , layer->params[MAT_N], layer->params[MAT_M]);
-        // printf ("Computed output for layer %d:\n", i);
-        // print_float_tensor (layer_output, input_params[BATCH], layer->params[NUM_HEAD]
-        //     , layer->params[MAT_N], layer->params[MAT_M]);
-        // printf ("Raw output for layer %d:\n", i);
-        // print_float_tensor (ldata_raw_output, input_params[BATCH], 1
-        //     , layer->params[MAT_N], layer->params[MAT_M]);
+    //     // printf ("Expected output for layer %d:\n", i);
+    //     // print_float_tensor (expected_output, input_params[BATCH], layer->params[NUM_HEAD]
+    //     //     , layer->params[MAT_N], layer->params[MAT_M]);
+    //     // printf ("Computed output for layer %d:\n", i);
+    //     // print_float_tensor (layer_output, input_params[BATCH], layer->params[NUM_HEAD]
+    //     //     , layer->params[MAT_N], layer->params[MAT_M]);
+    //     // printf ("Raw output for layer %d:\n", i);
+    //     // print_float_tensor (ldata_raw_output, input_params[BATCH], 1
+    //     //     , layer->params[MAT_N], layer->params[MAT_M]);
 
-        // if (layer->tensors[WEIGHT_TENSOR] != NULL)
-        // {
-        //     print_tensor_info (layer->tensors[WEIGHT_TENSOR], 1);
-        // }
-        compare_float_tensor (expected_output, ldata_output, 
-            input_params[BATCH], layer->params[OUT_C], layer->params[OUT_H],
-            layer->params[OUT_W], 1e-2, 1e-4, 20);
-        // compare_float_tensor (layer_output, ldata_output, 
-        //     input_params[BATCH],1, layer->params[MAT_N],
-        //     layer->params[MAT_M], 1e-2, 1e-4, 20);
-        compare_float_tensor (expected_output, layer_output, 
-            input_params[BATCH], layer->params[OUT_C], layer->params[OUT_H],
-            layer->params[OUT_W], 1e-2, 1e-4, 20);
+    //     // if (layer->tensors[WEIGHT_TENSOR] != NULL)
+    //     // {
+    //     //     print_tensor_info (layer->tensors[WEIGHT_TENSOR], 1);
+    //     // }
+    //     compare_float_tensor (expected_output, ldata_output, 
+    //         input_params[BATCH], layer->params[OUT_C], layer->params[OUT_H],
+    //         layer->params[OUT_W], 1e-2, 1e-4, 20);
+    //     // compare_float_tensor (layer_output, ldata_output, 
+    //     //     input_params[BATCH],1, layer->params[MAT_N],
+    //     //     layer->params[MAT_M], 1e-2, 1e-4, 20);
+    //     compare_float_tensor (expected_output, layer_output, 
+    //         input_params[BATCH], layer->params[OUT_C], layer->params[OUT_H],
+    //         layer->params[OUT_W], 1e-2, 1e-4, 20);
         
-        // free (expected_output);
-        // free (ldata_output);
-        // free (ldata_raw_output);
-        // free (layer_output);
-    }
+    //     // free (expected_output);
+    //     // free (ldata_output);
+    //     // free (ldata_raw_output);
+    //     // free (layer_output);
+    // }
 
     // LAYER_PARAMS output_order[] = {BATCH, OUT_H, OUT_W, OUT_C};
+    LAYER_PARAMS output_order[] = {BATCH, OUT_C, OUT_H, OUT_W};
     // LAYER_PARAMS output_order[] = {BATCH, MAT_N, MAT_M, 0};
-    // float *layer_output = ase_get_nasm_result (gpt2_nasm, output_order);
+    float *layer_output = ase_get_nasm_result (vgg16_nasm, output_order);
     // void *expected_output = load_arr ("data/gpt2/gpt2_128_layer0_output.bin", 128*1600*sizeof(float));
     // compare_float_array (expected_output, layer_output, 128*1600, 1e-2, 1e-4, 20);
-    // float *softmax_output = calloc (1000*resnet50_nasm->batch_size, sizeof(float));
-    // naive_softmax (layer_output, softmax_output, resnet50_nasm->batch_size, 1000);
+    float *softmax_output = calloc (1000*vgg16_nasm->batch_size, sizeof(float));
+    naive_softmax (layer_output, softmax_output, vgg16_nasm->batch_size, 1000);
     // // float *layer_output = get_aspen_tensor_data ((resnet50_dnn->layers + resnet50_dnn->num_layers - 1)->tensors[OUTPUT_TENSOR], output_order);
     // // print_float_array (layer_output, 1000*resnet50_nasm->batch_size, 1000);
-    // for (int i = 0; i < resnet50_nasm->batch_size; i++)
-    // {
-    //     get_probability_results ("data/imagenet_classes.txt", softmax_output + 1000*i, 1000);
-    // }
+    for (int i = 0; i < vgg16_nasm->batch_size; i++)
+    {
+        get_probability_results ("data/imagenet_classes.txt", softmax_output + 1000*i, 1000);
+    }
     // free (layer_output);
     // free (softmax_output);
 
