11ef0a9b61a43cf00d6a0f7800aaed735ce34259
chanjeong park <ckswjd99@snu.ac.kr>
2023-07-19 14:56:13 +0900

    release: layer pipelined & splitted offloading

--- scripts/run_schedule_experiment.sh
+++ scripts/run_schedule_experiment.sh
@@ -1,6 +1,6 @@
 ./main_scheduling \
     --sock_type=0   \
-    --sequential=1  \
+    --sequential=0  \
     --dirname=temp  \
     --target_config="data/cfg/vgg16_aspen.cfg"   \
     --target_nasm_dir="data/vgg16_B1_aspen.nasm" \
@@ -10,7 +10,7 @@
     --rx_ip="192.168.1.176" \
     --rx_port=3786   \
     --schedule_policy="sequential"  \
-    --sched_sequential_idx=5    \
+    --sched_sequential_idx=1    \
     --dse_num=16    \
     --output_order="cnn"    \
     --inference_repeat_num=10
