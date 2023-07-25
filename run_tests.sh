19b101cf4e03dee98c5ecf9d1ac7d9692b2e9211
cakeng <cakeng@naver.com>
2023-07-25 20:53:40 +0900

    fixes

--- run_tests.sh
+++ run_tests.sh
@@ -2,10 +2,10 @@
 
 # Take the first argument as the executable name
 
-num_dses=("1" "2" "4" "6" "8")
+num_dses=("4" "6" "8")
 dnn_list=("resnet50" "vgg16" "yolov3" "bert_base")
-batch_list=("1" "2" "4" "8" "16" "32")
-num_tiles=("20" "50" "100" "200")
+batch_list=("1" "2" "4" "8" "16")
+num_tiles=("20" "50" "100")
 total_runs=0
 current_run=0
 output_log="output.log"
@@ -37,19 +37,18 @@ do
                 current_run=$((current_run+1))
                 echo "    Running $dnn with $num_dse DSEs, batch size $batch, and $num_tile tiles ($current_run/$total_runs)"
                 echo "    Running $dnn with $num_dse DSEs, batch size $batch, and $num_tile tiles ($current_run/$total_runs)" >> $output_log
-                nasm_file="$dnn_B$batch_T$num_tile.nasm"
-                cmd="./$1 --sock_type=1 --pipelined=1 --dirname=temp \\ 
-                --target_nasm_dir=\"data/vgg16_B1.nasm\" \\
-                --target_dnn_dir=\"data/${dnn}_base.aspen\" \\
-                --prefix=\"$dnn\" \\
-                --rx_ip=\"127.0.01\" \\
-                --rx_port=3786 \\
-                --schedule_policy=\"local\" \\
-                --sched_sequential_idx=1 \\
-                --dse_num=$num_dse \\
-                --output_order=\"cnn\" \\
-                --inference_repeat_num=20\""
-                echo "    $cmd"
+                nasm_file="${dnn}_B${batch}_T${num_tile}.nasm"
+                cmd="./$1 --sock_type=2 --pipelined=1 --dirname=temp \
+                --target_nasm_dir=\"data/$nasm_file\" \
+                --target_dnn_dir=\"data/${dnn}_base.aspen\" \
+                --prefix=\"$dnn\" \
+                --rx_ip=\"127.0.0.1\" \
+                --rx_port=3786 \
+                --schedule_policy=\"local\" \
+                --sched_sequential_idx=1 \
+                --dse_num=$num_dse \
+                --output_order=\"cnn\" \
+                --inference_repeat_num=20"
                 echo "    $cmd" >> $output_log
                 $cmd >> $output_log
                 echo ""
