5346a4a0907fbf86da7b8847ee95c5fb811c94f3
Kmbin93 <kmbin@snu.ac.kr>
2023-08-14 13:44:57 +0900

    debug dynamic

--- include/nasm.h
+++ include/nasm.h
@@ -91,8 +91,8 @@ struct ninst_t
     int dse_idx;
 
     // For Scheduling
-    atomic_int dev_to_compute [SCHEDULE_MAX_DEVICES];       // who will compute this ninst?
-    atomic_int dev_send_target [SCHEDULE_MAX_DEVICES];    // who wants the result of this ninst?
+    atomic_int dev_to_compute [SCHEDULE_MAX_DEVICES+1];       // who will compute this ninst?
+    atomic_int dev_send_target [SCHEDULE_MAX_DEVICES+1];    // who wants the result of this ninst?
     atomic_int offloaded;
 
     double compute_start;
